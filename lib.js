de4f32928ab5215527a71404643dfcebd4c7eb25
jnpn <johan.ponin.pro@gmail.com>
2022-07-12 16:52:13 +0200

    feat(lib): return dummy values

--- lib.js
+++ lib.js
@@ -2,11 +2,11 @@
 //
 // interval(N,A,B) renvoie true is A < N < B sinon false
 
-function interval(n,a,b) {}
+function interval(n,a,b) { return -1; }
 
 // fonction factorielle
 //
 // factorielle nombre = 1 si nombre inferieur a 2
 //               nombre * factorielle(nombre-1)
 
-function factorielle(nombre) {}
+function factorielle(nombre) { return -1; }
