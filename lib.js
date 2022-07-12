7c75dac229dfe7f615812cfa60d9c1e038696af9
jnpn <johan.ponin.pro@gmail.com>
2022-07-12 15:53:41 +0200

    feat(lib): create lib.js

---
+++ lib.js
@@ -0,0 +1,12 @@
+// fonction interval
+//
+// interval(N,A,B) renvoie true is A < N < B sinon false
+
+function interval(n,a,b) {}
+
+// fonction factorielle
+//
+// factorielle nombre = 1 si nombre inferieur a 2
+//               nombre * factorielle(nombre-1)
+
+function factorielle(nombre) {}
